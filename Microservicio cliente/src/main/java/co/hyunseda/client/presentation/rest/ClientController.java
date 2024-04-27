c0c869cedfd0bf0bbbe3f5e05aa4d2a7f03d44e0
Yesid Obando <eiderobamdo@gmail.com>
2024-04-27 18:41:20 -0500

    Microservicio cliente agregado

--- Microservicio cliente/src/main/java/co/hyunseda/client/presentation/rest/ClientController.java
+++ Microservicio cliente/src/main/java/co/hyunseda/client/presentation/rest/ClientController.java
@@ -2,7 +2,10 @@ package co.hyunseda.client.presentation.rest;
 
 import co.hyunseda.client.Domain.Entity.Client;
 import co.hyunseda.client.Domain.Service.IClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,8 @@ import java.util.List;
 @RequestMapping("client")
 public class ClientController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
+
     @Autowired
     private IClientService clientService;
 
@@ -20,8 +25,12 @@ public class ClientController {
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
     public boolean saveClient(@RequestBody Client client) {
-        return  (clientService.save(client)) ;
+        // Log para depurar el objeto Client recibido en la solicitud POST
+        logger.debug("Cliente recibido en la solicitud POST: {}", client);
+
+        return clientService.save(client);
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Client> getClientById(@PathVariable Long id) {
@@ -33,15 +42,10 @@ public class ClientController {
         }
     }
 
-    @GetMapping
-    public ResponseEntity<List<Client>> getAllClients() {
-        List<Client> clients = clientService.findAll();
-        if (!clients.isEmpty()) {
-            return ResponseEntity.ok(clients);
-        } else {
-            return ResponseEntity.noContent().build();
-        }
-    }
+       @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+       public List<Client> findAllClients(){
+        return clientService.findAll();
+       }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateClient(@PathVariable Long id, @RequestBody Client client) {
